edf841f194529f9c4acae232a671aa14348cbff0
oraulinepuig <oceane.raulinepuig@gmail.com>
2023-12-02 16:25:24 +0100

    modifications sur export - en cours de debug mais j'y suis presque

--- buildins/export.c
+++ buildins/export.c
@@ -6,7 +6,7 @@
 /*   By: orauline <orauline@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 14:06:37 by orauline          #+#    #+#             */
-/*   Updated: 2023/12/02 15:08:36 by orauline         ###   ########.fr       */
+/*   Updated: 2023/12/02 16:23:55 by orauline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,17 @@ static void	add_to_env(char *cmd, t_env *env)
 		;
 	updated_env = malloc(sizeof(char *) * (len + 2));
 	if (!updated_env)
+	{
+		free(updated_env);
 		return ;
+	}
 	updated_env[len] = ft_strdup(cmd);
 	updated_env[len + 1] = NULL;
 	while (env->env_cpy[++i])
+	{
 		updated_env[i] = ft_strdup(env->env_cpy[i]);
+		free(env->env_cpy[i]);
+	}
 	free(env->env_cpy);
 	env->env_cpy = updated_env;
 }
@@ -36,20 +42,25 @@ static void	add_to_env(char *cmd, t_env *env)
 static void	process_key(char *key, t_env *env)
 {
 	char	*equal;
+	char	*new_key;
+	char	*temp;
+	char	*equal_not_found;
 
 	equal = ft_strchr(key, '=');
 	if (!equal)
 	{
-		equal = "=''";
-		key = ft_strncat(ft_strdup(key), equal, ft_strlen(equal));
-		add_to_env(key, env);
-		free(key);
+		equal_not_found = "=''";
+		new_key = ft_strncat(ft_strdup(key), equal_not_found, ft_strlen(equal_not_found));
+		add_to_env(new_key, env);
+		free(new_key);
 	}
 	else
 	{
+		temp = ft_strdup(key);
 		*equal = '\0';
-		add_to_env(key, env);
+		add_to_env(temp, env);
 		*equal = '=';
+		free(temp);
 	}
 }
 
@@ -58,7 +69,7 @@ static void	update_env(char *cmd, t_env *env)
 	int		i;
 	char	*key;
 
-	if (check_digit(cmd))
+	if (!cmd || check_digit(cmd))
 	{
 		printf("export: not an identifier: %s\n", cmd);
 		return ;
@@ -76,7 +87,7 @@ static void	update_env(char *cmd, t_env *env)
 			return ;
 		}
 	}
-	// free(key);
+	free(key);
 	add_to_env(cmd, env);
 }
 
@@ -85,6 +96,8 @@ int	m_export(char **cmds, t_env *env)
 	int	j;
 
 	j = 0;
+	if (!cmds)
+		return (0);
 	if (!cmds[1])
 	{
 		while (env->env_cpy[j])
